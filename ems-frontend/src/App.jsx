d31a0fe3c832077e551695ed454cb78e24c3d696
Taay Sosa <mateoghost3@gmail.com>
2025-11-25 05:54:10 +0200

    Configure Routing for React App

--- ems-frontend/src/App.jsx
+++ ems-frontend/src/App.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
-import ListEmployee from "./component/ListEmployee.jsx";
 import Header from "./component/Header.jsx";
+import ListEmployee from "./component/ListEmployee.jsx";
 import Footer from "./component/Footer.jsx";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 const App = () => {
     return (
         <>
-            <Header />
-            <ListEmployee />
-            <Footer />
+            <BrowserRouter>
+                <Header/>
+                <Routes>
+                    {/*http://localhost:3000*/}
+                    <Route path="/" element={<ListEmployee />}/>
+                    {/*http://localhost:3000/employees*/}
+                    <Route path="/employees" element={<ListEmployee />}/>
+                </Routes>
+                <Footer/>
+            </BrowserRouter>
         </>
     )
 }
